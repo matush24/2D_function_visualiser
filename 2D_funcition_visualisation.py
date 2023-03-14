a54678bc80e410e9eb48adcc699f0ad2d414303a
Matúš <49812517+matush24@users.noreply.github.com>
2023-03-14 20:12:02 +0100

    template

--- 2D_funcition_visualisation.py
+++ 2D_funcition_visualisation.py
@@ -5,25 +5,12 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 
 # define 2d function
-def f(tau, T):
-   # tau = 0.5
-   t0 = 1
-   x=(2*t0-T)/tau
-   x1=(t0-T)/tau
-   x2=(-T)/tau
-   xi = (np.arctan(x) + np.arctan(-x2))**-1
-   return xi*((2*t0-T)*(np.arctan(x)-np.arctan(x1))-\
-   (tau/2)*(np.log(1+x**2)-np.log(1+x1**2))+\
-   (t0-T)*(np.arctan(x1)-np.arctan(x2))-\
-   (tau/2)*(np.log(1+x1**2)-np.log(1+x2**2)))
-
-   # return tau**-1 * xi * (1/(1 + ((t-T)/tau)**2)) 
-
-print(f(0.4, 1))
+def f(x, y):
+   return x*y
 
 # set area
-xmin, xmax = 0, 2
-ymin, ymax = 0, 2
+xmin, xmax = -1, 1
+ymin, ymax = -1, 1
 
 # set resolution for wireframe (intervals to devide (xmin, xmax), (ymin, ymax) into)
 xresw, yresw = 30, 60
@@ -48,16 +35,16 @@ fig, axs = plt.subplots(1, 2, subplot_kw={"projection": "3d"})
 
 axs[0].plot_wireframe(Xw, Yw, Zw)
 axs[0].set_title('Wireframe')
-axs[0].set_xlabel('tau')
-axs[0].set_ylabel('T')
+axs[0].set_xlabel('x')
+axs[0].set_ylabel('y')
 axs[0].set_zlabel('f(x)')
 
 surf = axs[1].plot_surface(Xh, Yh, Zh, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 axs[1].zaxis.set_major_locator(LinearLocator(10))
 fig.colorbar(surf, shrink=0.4, aspect=50)
 axs[1].set_title('Heatmap')
-axs[1].set_xlabel('tau')
-axs[1].set_ylabel('T')
+axs[1].set_xlabel('x')
+axs[1].set_ylabel('y')
 axs[1].set_zlabel('f(x)')
 
 plt.show()
